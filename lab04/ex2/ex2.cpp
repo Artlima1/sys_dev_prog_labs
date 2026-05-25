b57075f7e4b228b7ad17ceb003888f165aca31cf
Artlima1 <arthurdeolima@gmail.com>
2026-05-25 16:30:17 +0200

    Incomplete version C

--- lab04/ex2/ex2.cpp
+++ lab04/ex2/ex2.cpp
@@ -15,11 +15,12 @@ int main(int argc, char * argv[]) {
     auto v1 = new double[5]{ -0.0613, -0.1184,  0.2655,  0.2952, -0.3131 };
     auto v2 = new double[5]{ -0.3235,  0.1948, -0.1829,  0.4502, -0.4656 };
     auto mat = new double*[5];
-    mat[0] = new double[5]{ -0.3424, -0.3581,  0.1557,  0.2577,  0.2060 };
-    mat[1] = new double[5]{  0.4706, -0.0782, -0.4643,  0.2431, -0.4682 };
-    mat[2] = new double[5]{  0.4572,  0.4157,  0.3491,  0.1078, -0.2231 };
-    mat[3] = new double[5]{ -0.0146,  0.2922,  0.4340, -0.1555, -0.4029 };
-    mat[4] = new double[5]{  0.3003,  0.4595,  0.1787, -0.3288, -0.4656 };
+    // stored transposed: mat[i] = i-th column of M, enabling cache-friendly row access
+    mat[0] = new double[5]{ -0.3424,  0.4706,  0.4572, -0.0146,  0.3003 };
+    mat[1] = new double[5]{ -0.3581, -0.0782,  0.4157,  0.2922,  0.4595 };
+    mat[2] = new double[5]{  0.1557, -0.4643,  0.3491,  0.4340,  0.1787 };
+    mat[3] = new double[5]{  0.2577,  0.2431,  0.1078, -0.1555, -0.3288 };
+    mat[4] = new double[5]{  0.2060, -0.4682, -0.2231, -0.4029, -0.4656 };
 #else
     n = std::stoi(argv[1]);
     auto v1 = build_rand_array(n);
--- lab04/ex2/utils.cpp
+++ lab04/ex2/utils.cpp
@@ -1,5 +1,12 @@
 
 #include <cstdlib>
+#include <future>
+#include <thread>
+#include <vector>
+
+// #define VERSION_SEQ
+//#define VERSION_A
+#define VERSION_C
 
 double * build_rand_array(const int size) {
     auto array = new double[size];
@@ -31,16 +38,43 @@ void delete_matrix(double ** m, const int size) {
     delete[] m;
 }
 
-double op(const double * v1, const double * v2, double ** mat, const int n) {
+void vec(const double  * v1, const double * v2, int size, double * res) {
+    double tmp = 0;
+    for (int i = 0; i < size; i++) {
+        tmp += v1[i] * v2[i];
+    }
+    *res = tmp;
+}
+
+double op_seq(const double * v1, const double * v2, double ** mat, const int n) {
     double tmp = 0;
     const auto v = new double[n];
 
-    for (int j = 0; j < n; j++) {
-        tmp = 0;
-        for (int i = 0; i < n; i++) {
-            tmp += v1[i] * mat[i][j];
-        }
-        v[j] = tmp;
+    for (int i = 0; i < n; i++) {
+        vec(v1, mat[i], n, &v[i]);
+    }
+
+
+    tmp = 0;
+    for (int i = 0; i < n; i++) {
+        tmp += v2[i] * v[i];
+    }
+
+    delete[] v;
+    return tmp;
+}
+
+double op_vA(const double * v1, const double * v2, double ** mat, const int n) {
+    double tmp = 0;
+    const auto v = new double[n];
+
+    std::vector<std::thread> pool;
+    for (int i = 0; i < n; i++) {
+        pool.emplace_back([&, i] {vec(v1, mat[i], n, &v[i]);});
+    }
+
+    for (int i = 0; i < n; i++) {
+        pool[i].join();
     }
 
     tmp = 0;
@@ -51,3 +85,42 @@ double op(const double * v1, const double * v2, double ** mat, const int n) {
     delete[] v;
     return tmp;
 }
+
+double op_vC(const double * v1, const double * v2, double ** mat, const int n) {
+    double tmp = 0;
+    const auto v = new std::future<double>[n];
+
+    for (int i = 0; i < n; i++) {
+        v[i] = std::async (
+            std::launch::async,
+            [&, i]() {
+                double tmp_l = 0;
+                for (int j = 0; j < n; j++) {
+                    tmp_l += v1[j] * mat[i][j];
+                }
+                return tmp_l;
+            }
+        );
+    }
+
+    tmp = 0;
+    for (int i = 0; i < n; i++) {
+        tmp += v2[i] * v[i].get();
+    }
+
+    delete[] v;
+    return tmp;
+}
+
+double op(const double * v1, const double * v2, double ** mat, const int n) {
+#ifdef VERSION_SEQ
+    return op_seq(v1, v2, mat, n);
+#elif defined VERSION_A
+    return op_vA(v1, v2, mat, n);
+#elif defined VERSION_C
+    return op_vC(v1, v2, mat, n);
+#else
+    return 0;
+#endif
+}
+
